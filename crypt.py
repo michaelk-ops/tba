d7b89fb81030f14ac5d0b8979ca2fa7d77a29e27
Gerco van Heerdt <gercovanheerdt@gmail.com>
2025-03-24 09:07:49 +0000

    Rares

--- crypt.py
+++ crypt.py
@@ -2,8 +2,10 @@
 
 from automate import Automator
 import time
+import argparse
 
-crypts = ["crypts/" + name for name in ["cave", "cog", "dune", "greek", "grotto", "ice", "lava", "rune", "skull", "snake", "temple", "pyramid"]]
+common_crypts = ["crypts/" + name for name in ["cave", "cog", "dune", "greek", "grotto", "ice", "lava", "rune", "skull", "snake", "temple", "pyramid"]]
+rare_crypts = ["crypts/" + name for name in ["left", "right"]]
 
 def crypt(a):
     if not a.await_samples("watchtower", 2):
@@ -18,6 +20,7 @@ def crypt(a):
     if not a.random_multi():
         return False
     a.click()
+    a.move(400, 0)
     if not a.await_samples_multi(crypts, 4):
         return False
     a.multi_block_color(120, 90, (48, 37, 130))
@@ -29,6 +32,12 @@ def crypt(a):
     time.sleep(0.3)
     if not a.await_samples("carter_selected", 2):
         return False
+    if not a.await_samples("selected", 2):
+        return False
+    if rare:
+        if not a.await_samples("rare_open", 2):
+            return False
+        a.click()
     if not a.await_samples("explore", 2):
         return False
     a.click(0, 10)
@@ -46,6 +55,22 @@ def speedup(a):
         time.sleep(2)
         return True
     return False
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--type", default = "common", help = "crypt type")
+args = parser.parse_args()
+
+crypt_type = args.type
+
+rare = False
+if crypt_type == "common":
+    crypts = common_crypts
+elif crypt_type == "rare":
+    crypts = rare_crypts
+    rare = True
+else:
+    print("Unknown crypt type")
+    exit(1)
 
 a = Automator()
 fails = 0
